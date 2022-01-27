eebf270d0acb1b989e34f296939ed7d588af0cbe
Rednyss <98532857+Rednyss@users.noreply.github.com>
2022-01-27 15:27:58 +0200

    Add files via upload

---
+++ phpproject/data/items.php
@@ -0,0 +1,9 @@
+<?php 
+
+$bagazas = range(1, 15);
+array_push($bagazas, "15+");
+$skrydzioRandNr = [rand(100, 1000000000), rand(100, 1000000000), rand(100, 1000000000), rand(100, 1000000000), rand(100, 1000000000)];
+
+$countries = ['Russia', 'Canada', 'Japan', 'Lithuania', 'Poland'];
+
+
